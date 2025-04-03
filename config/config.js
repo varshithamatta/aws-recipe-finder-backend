c6dab3d2783ceef680b842871a2474a738ed7dcb
varshitha matta <varshitham41@gmail.com>
2025-04-03 10:01:12 +0530

    Hosting in AWS

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config(); // Load environment variables from .env
+
+module.exports = {
+  development: {
+    username: "root",
+    password: "12345",
+    database: "recipe_finder_db",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+  }
+};
