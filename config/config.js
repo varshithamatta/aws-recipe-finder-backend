5f8abba297dfe37f0f6ea7d2121f53acfdd1e5a0
varshitha matta <varshitham41@gmail.com>
2025-04-03 10:21:40 +0530

    COnnected string url

--- config/config.js
+++ config/config.js
@@ -1,25 +1,12 @@
-require('dotenv').config(); // Load environment variables from .env
+require("dotenv").config(); // Load environment variables
 
 module.exports = {
   development: {
-    username: "root",
-    password: "12345",
-    database: "recipe_finder_db",
-    host: "127.0.0.1",
-    dialect: "mysql"
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    use_env_variable: "MYSQL_URL",
+    dialect: "mysql",
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: "mysql"
+    use_env_variable: "MYSQL_URL",
+    dialect: "mysql",
   }
 };
